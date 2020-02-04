11d4629b251f03bb6cc190264c62c3db677998d2
zyx172 <zyx172571@163.com>
2020-02-04 10:39:47 +0800

    注释

--- microservicecloud-eureka-7002/src/main/java/com/atguigu/springcloud/EurekaServer7002_App.java
+++ microservicecloud-eureka-7002/src/main/java/com/atguigu/springcloud/EurekaServer7002_App.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+
 @EnableEurekaServer // EurekaServer服务器端启动类,接受其它微服务注册进来
 public class EurekaServer7002_App
 {
+	//这是主启动类
 	public static void main(String[] args)
 	{
 		SpringApplication.run(EurekaServer7002_App.class, args);
